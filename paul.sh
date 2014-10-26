59ac610cd3c0ce8dc148cb3a80f7ec8a664afe37
X00107801 <X00107801@gmail.com>
2014-10-25 22:06:51 -0500

    this is my new test shell file

---
+++ paul.sh
@@ -0,0 +1,83 @@
+ 
+# test case 1
+# inputs 100 100
+
+# outputs expected   pass with distinction
+# actual outputs     pass with distinction
+
+# result test passed 
+
+# test case 2
+# inputs 30 50
+
+# outputs expected   component fail
+# actual outputs     component fail
+
+# result test passed 
+
+# test case 3
+# inputs 40 50
+
+# outputs expected   fail
+# actual outputs     fail
+
+# result test passed 
+
+# test case 4
+# inputs 50 80
+
+# outputs expected   pass 
+# actual outputs     pass 
+
+# result test passed 
+
+# test case 5
+# inputs 60 60
+
+# outputs expected   pass 
+# actual outputs     pass 
+
+# result test passed 
+
+# test case 6
+# inputs 39 100
+
+# outputs expected   component fail
+# actual outputs     component fail
+
+# result test passed 
+
+# test case 7
+# inputs 110 100
+
+# outputs expected   invalid entry
+# actual outputs     invalid entry
+
+# result test passed 
+
+# test case 8
+# inputs 50 50
+
+# outputs expected   pass 
+# actual outputs     fail
+
+# result test failed  
+#  issue needs to be returned for attention
+
+# test case 9
+# inputs characters p and q
+
+# outputs expected   invalid entry
+# actual outputs     exception in thread "main" java.NumberFormatException
+
+# results test failed
+# code needs to be modified to output 'invalid entry' message
+
+# test case 10
+# inputs no parameters entered after java grades
+
+# expected outcome   PLEASE enter values
+# actual outcome     out of bounds exception error
+
+# test failure
+# A please enter values message would make things clearer 
